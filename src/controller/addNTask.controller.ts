b451f54f6621a3c9640102922b4450d643d7e2b4
John Raymond <john.raymond.blando@gmail.com>
2025-11-09 10:17:51 +0800

    R-12: Fix digit that result to 2 digit from adding 1

--- src/controller/addNTask.controller.ts
+++ src/controller/addNTask.controller.ts
@@ -1,18 +1,23 @@
-import { Request, Response, Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
 import { nGenerateRandomInt, nAddOnePerDigit } from '../util/addNTask.util';
 
 const addNTaskController = Router();
 
-addNTaskController.get('/randomnumber', async (req: Request, res: Response) => {
+addNTaskController.get('/randomnumber', async (
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
   try {
     const randomNumber = await nGenerateRandomInt(1000, 10000);
     console.log(randomNumber);
     const randomNumberAddOne = nAddOnePerDigit(randomNumber, 1);
     console.log(randomNumberAddOne);
 
+    // objects are treated as JSON content type
     res.send({
-      randomNumber,
-      randomNumberAddOne
+      number: `${randomNumber}`,
+      answer: randomNumberAddOne
     });
   } catch (err) {
     const errorMsg = `Error generating random number: ${err}`;
